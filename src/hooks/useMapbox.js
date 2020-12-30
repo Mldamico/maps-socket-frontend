14fdf91fbd72c58e429c52499bc5b5512d316e64
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2020-12-30 08:23:02 -0300

    Agregar marcadores

--- src/hooks/useMapbox.js
+++ src/hooks/useMapbox.js
@@ -1,49 +1,49 @@
-import { useRef, useEffect, useCallback, useState } from "react";
-import mapboxgl from "mapbox-gl";
-import { v4 } from "uuid";
+import { useRef, useEffect, useCallback, useState } from 'react';
+import mapboxgl from 'mapbox-gl';
+import { v4 } from 'uuid';
 
-export const useMapbox = puntoInicial => {
+export const useMapbox = (puntoInicial) => {
   const mapaDiv = useRef();
-  const setRef = useCallback(node => {
+  const setRef = useCallback((node) => {
     mapaDiv.current = node;
   }, []);
   const mapa = useRef(null);
   const [coords, setCoords] = useState(puntoInicial);
   const marcadores = useRef({});
+
+  const agregarMarcador = useCallback((ev) => {
+    const { lng, lat } = ev.lngLat;
+    const marker = new mapboxgl.Marker();
+    marker.id = v4();
+    marker.setLngLat([lng, lat]).addTo(mapa.current).setDraggable(true);
+    marcadores.current[marker.id] = marker;
+  }, []);
+
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: mapaDiv.current,
-      style: "mapbox://styles/mapbox/streets-v11",
+      style: 'mapbox://styles/mapbox/streets-v11',
       center: [puntoInicial.lng, puntoInicial.lat],
-      zoom: puntoInicial.zoom
+      zoom: puntoInicial.zoom,
     });
     mapa.current = map;
   }, [puntoInicial]);
 
   useEffect(() => {
-    mapa.current?.on("move", () => {
+    mapa.current?.on('move', () => {
       const { lng, lat } = mapa.current.getCenter();
       setCoords({
         lng: lng.toFixed(4),
         lat: lat.toFixed(4),
-        zoom: mapa.current.getZoom().toFixed(2)
+        zoom: mapa.current.getZoom().toFixed(2),
       });
     });
-    return mapa.current?.off("move");
+    return mapa.current?.off('move');
   }, []);
 
   useEffect(() => {
-    mapa.current?.on("click", ev => {
-      const { lng, lat } = ev.lngLat;
-      const marker = new mapboxgl.Marker();
-      marker.id = v4();
-      marker
-        .setLngLat([lng, lat])
-        .addTo(mapa.current)
-        .setDraggable(true);
-      marcadores.current[marker.id] = marker;
-    });
-  }, []);
+    mapa.current?.on('click', agregarMarcador);
+  }, [agregarMarcador]);
 
-  return { coords, setRef, marcadores };
+  return { agregarMarcador, coords, setRef, marcadores };
 };
